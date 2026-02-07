44dbc9d3ff79f30b47e95283f99c3bc5a4e212c8
I-am-Emmanuel <olaifaemmanueloluwatobiloba@gmail.com>
2026-02-07 13:16:01 +0100

    data analysis and machine learning code

---
+++ steps.py
@@ -0,0 +1,18 @@
+
+# Data analysis is the process of inspecting, 
+# cleaning, transforming, and interpreting data in order to discover 
+# useful information, draw conclusions, and support decision-making. 
+
+# Introduction
+# setting up
+# python basic
+# using python libraries
+# simple visualization
+
+# Machine learning steps
+# import data
+# clean data
+# split data. Training set(80%)/test set(20%)
+# create a Model
+# check the output
+# improve
